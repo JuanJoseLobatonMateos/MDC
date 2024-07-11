86563fc6c64ca66a76a950de5c4531b940dbf2cd
Juan Lobatón <jlobatonm@gmail.com>
2024-07-12 00:21:12 +0200

    commit

--- app/src/main/java/com/jlobatonm/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/jlobatonm/mdc/ScrollingActivity.kt
@@ -3,9 +3,6 @@ package com.jlobatonm.mdc
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.jlobatonm.mdc.databinding.ActivityScrollingBinding
 
@@ -17,17 +14,9 @@ class ScrollingActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        
         binding = ActivityScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
     }
     
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
