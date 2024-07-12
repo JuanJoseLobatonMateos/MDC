93288f1cee169176fa98ea90fb28ed122b230be2
Juan Lobatón <jlobatonm@gmail.com>
2024-07-12 13:46:54 +0200

    commit

--- app/src/main/java/com/jlobatonm/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/jlobatonm/mdc/ScrollingActivity.kt
@@ -3,7 +3,10 @@ package com.jlobatonm.mdc
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
 import com.jlobatonm.mdc.databinding.ActivityScrollingBinding
 
 class ScrollingActivity : AppCompatActivity()
@@ -17,6 +20,23 @@ class ScrollingActivity : AppCompatActivity()
         binding = ActivityScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
+        binding.fab.setOnClickListener {
+            if(binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            }else{
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }
+        
+        binding.bottomAppBar.setNavigationOnClickListener {
+            Snackbar.make(binding.root, getString(R.string.message_action_success),Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .show()
+        }
+        
+        binding.content.btnSkip.setOnClickListener {
+            binding.content.cdAdd.visibility   = View.GONE
+        }
     }
     
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
